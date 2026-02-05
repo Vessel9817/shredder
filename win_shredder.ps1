e6443ffc439b5b2d73ed4c9383b1bbff0b838873
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-02-05 15:36:53 -0500

    Cleaned up and fixed eval command bug
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- win_shredder.ps1
+++ win_shredder.ps1
@@ -1,54 +1,44 @@
-# Escapes single quotes for Linux
-function Format-Quotes-Linux() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string]$Str
-    )
+# $DebugPreference = 'Continue'
 
+# Resolves the issue of Write-Debug wrapping text
+# https://stackoverflow.com/a/4103885
+function global:Write-Debug([string] $Message) {
+    if ($DebugPreference -ne 'SilentlyContinue') {
+        Write-Host "DEBUG: $Message" -ForegroundColor 'Yellow'
+    }
+}
+
+# Escapes single quotes for Linux
+function Format-Quotes-Linux([string] $Str) {
     return $Str.Replace("'", "'`"'`"'")
 }
 
 # Escapes dollar signs for WSL CLI usage
-function Format-Dollars-Linux() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string]$Str
-    )
-
+function Format-Dollars-Linux([string] $Str) {
     return $Str.Replace('$', '\$')
 }
 
 # Escapes backtick and dollar signs for PowerShell
-function Format-PowerShell() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string]$Str
-    )
-
+function Format-PowerShell([string] $Str) {
     # Order is important!
     return $Str.Replace('\', '\\').Replace('`', '\`')
 }
 
 # Converts Windows paths to WSL paths
-function ConvertTo-Linux-Paths() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string[]]$Paths
-    )
-
+function ConvertTo-Linux-Paths([string[]] $Paths) {
     # Preventing ACE by escaping paths
     for ($i = 0; $i -lt $Paths.Length; $i++) {
         $Paths[$i] = Format-PowerShell (Format-Quotes-Linux $Paths[$i])
     }
 
     # Formatting conversion command
+    # Single quotes are necessary, otherwise $() substitution is a valid ACE target
     $setPathsCmd = "paths=('" + ($Paths -join "' '") + "')"
-    $pathCmd = "$setPathsCmd; for ((i = 0; i < `${#paths[@]}; i++)); do wslpath -u `"`${paths[`$i]}`" 2>/dev/null; done"
+    $pathCmd = "$setPathsCmd; for ((i = 0; i < `${#paths[@]}; i++)); do wslpath -u \`"`${paths[`$i]}\`" 2>/dev/null; done"
     $pathCmd = Format-Dollars-Linux $pathCmd
+
+    Write-Debug "Generated WSL command:`n$pathCmd"
+    Write-Debug "What WSL sees:`n$(wsl -- echo $pathCmd)"
 
     # Performing conversion
     $paths = (wsl -- eval $pathCmd).Split("`n")
@@ -62,13 +52,7 @@ function ConvertTo-Linux-Paths() {
 }
 
 # Securely removes files and folders
-function Remove-And-Shred-Files() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string[]]$LinuxPaths
-    )
-
+function Remove-And-Shred-Files([string[]] $LinuxPaths) {
     foreach ($path in $LinuxPaths) {
         wsl -- eval "find $path -type f -exec shred -uvz {} +"
     }
@@ -77,13 +61,7 @@ function Remove-And-Shred-Files() {
 }
 
 # The script entrypoint
-function Main() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string[]]$WindowsPaths
-    )
-
+function Main([string[]] $WindowsPaths) {
     # Error handling
     if ($WindowsPaths.Length -lt 1) {
         Write-Error "Usage: win_shredder [paths...]"
@@ -93,7 +71,7 @@ function Main() {
     # Fully qualifying paths
     $absPaths = Convert-Path -LiteralPath $WindowsPaths
 
-    if ($absPaths -eq $null) {
+    if ($null -eq $absPaths) {
         Write-Error "Failed to parse paths. See above error."
         return
     }
